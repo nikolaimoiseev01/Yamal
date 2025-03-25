7e7aeb013c9f74d4aebe240fef570adcc1f6ecb1
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-03-25 13:24:37 +0300

    minor

--- app/Livewire/Components/Portal/ContactForm.php
+++ app/Livewire/Components/Portal/ContactForm.php
@@ -24,7 +24,7 @@ class ContactForm extends Component
 
         $subject = 'Сообщение с сайта';
         $question = 'Вопрос: ' . $this->question;
-        Mail::to('tomas232@mail.ru')->send(new \App\Mail\ContactForm($this->name, $this->contact, $this->question));
+        Mail::to('dapk-gourmet@yanao.ru')->send(new \App\Mail\ContactForm($this->name, $this->contact, $this->question));
         $this->sent = True;
     }
 }
