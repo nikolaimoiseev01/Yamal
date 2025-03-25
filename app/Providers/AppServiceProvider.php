b139f22b909bfef5bbe619f20c6008166c90f0ed
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-03-25 12:52:47 +0300

    minor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production' && !str_contains(request()->path(), 'mailru-domainCebaXLOXCOlUVq5r')) {
-            \URL::forceScheme('https');
-        }
+//        if (config('app.env') === 'production') {
+//            URL::forceScheme('https');
+//        }
     }
 }
